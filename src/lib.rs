388e4e904dbb3fc54b333ad8c8530083effbefe1
Corey Cothrum <coreycothrum@melloyawn.com>
2023-09-20 12:56:22 -0500

    feat(Entrypoint): init trait
    w/ default impls that do almost nothing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
+//! #FIXME
+
 #![forbid(unsafe_code)]
 
 pub use anyhow;
 pub use clap;
 pub use tracing;
 
-pub trait Entrypoint: clap::Parser {}
+use anyhow::Result;
+use clap::Parser;
+use tracing::info;
+
+/// #FIXME
+pub trait Entrypoint: Parser {
+    fn additional_startup_config(self) -> Result<Self> {
+        Ok(self)
+    }
+
+    /// #FIXME
+    fn entrypoint<F>(self, function: F) -> Result<()>
+    where
+        F: FnOnce(Self) -> Result<()>,
+    {
+        let entrypoint = { self.additional_startup_config()? };
+
+        info!("executing entrypoint() function");
+        function(entrypoint)
+    }
+}
+impl<P: Parser> Entrypoint for P {}
