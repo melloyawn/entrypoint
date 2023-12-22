a92dbf204ff757c9012459e6a56b41f7f0cf1a90
Corey Cothrum <coreycothrum@melloyawn.com>
2023-12-21 20:20:16 -0600

    docs: WIP

--- entrypoint/README.md
+++ entrypoint/README.md
@@ -13,19 +13,19 @@ eliminate main function boilerplate with this opinionated application framework/
 
 ### What does this crate actually do?
 `entrypoint` wraps a user defined function with automatic configuration/setup/processing of:
-- logging (via [`tracing`](https://github.com/tokio-rs/tracing))
+- easy application error handling (via [`anyhow`](https://github.com/dtolnay/anyhow))
 - command-line argument parsing (via [`clap`](https://github.com/clap-rs/clap))
 - `.dotenv` file processing and environment variable population/overrides (via [`dotenvy`](https://github.com/allan2/dotenvy))
-- easy application error handling (via [`anyhow`](https://github.com/dtolnay/anyhow))
+- logging (via [`tracing`](https://github.com/tokio-rs/tracing))
 
 The user defined function is intended to be/replace `main()`.
 
 Meaning, this main/entrypoint function can be written as if all the configuration/processing/boilerplate is ready-to-use.
 More explicitly:
+- `anyhow` is available and ready to use
 - `clap::Parser` struct has been parsed and populated
 - `.dotenv` files have been parsed; environment variables are ready to go
 - `tracing` has been configured and the global subscriber has been registered
-- `anyhow` is available and ready to use
 
 ## Usage
 ### Default Config
@@ -49,14 +49,14 @@ More explicitly:
     ```rust
     #[entrypoint::entrypoint]
     fn entrypoint(args: CLIArgs) -> entrypoint::anyhow::Result<()> {
+        // args are parsed and ready to use
+        info!("verbose set to: {:?}", args.verbose);
+
         // env::vars() already loaded-from/merged-with .dotenv file(s)
         let _my_var = env::vars("SOMETHING_FROM_DOTENV_FILE");
 
         // logging is ready to use
         info!("entrypoint::entrypoint");
-
-        // args are parsed and ready to use
-        info!("verbose set to: {:?}", args.verbose);
 
         Ok(())
     }
@@ -67,7 +67,7 @@ Using the default behavior is totally reasonable, but [overwriting some default 
 
 ### Usage Notes
 1. The `entrypoint` function must:
-   1. accept a `clap::Parser` as an input
+   1. Have a `clap::Parser` input parameter
    2. return `entrypoint::anyhow::Result<()>`
 2. `#[entrypoint::entrypoint]` ordering may matter when used with other attribute macros (e.g. `[tokio::main]`).
 
--- entrypoint/src/lib.rs
+++ entrypoint/src/lib.rs
@@ -1,20 +1,67 @@
-//! entrypoint: an app wrapper to eliminate startup boilerplate
-
+//! an (opinionated) app wrapper to eliminate main function boilerplate
+//!
+//! Eliminate boilerplate by smartly integrating:
+//! * [`anyhow`](https://crates.io/crates/anyhow): for easy error handling
+//! * [`clap`](https://crates.io/crates/clap): for easy CLI parsing
+//! * [`dotenv`](https://crates.io/crates/dotenv): for easy environment variable management
+//! * [`tracing`](https://crates.io/crates/tracing): for easy logging
+//!
+//! In lieu of `main()`, an [`entrypoint`] function is defined.
+//!
+//! Perfectly reasonable setup/config is done automagically.
+//! More explicitly, the [`entrypoint`] function can be written as if:
+//! * [`anyhow::Error`] is ready to propogate
+//! * CLI have been parsed
+//! * `.dotenv` files have already been processed and populated into the environment
+//! * logging is ready to use
+//!
+//! Customization can be acheived by overriding various [trait](#traits) default implementations.
+//!
+//! # Examples
+//! ```
+//! use entrypoint::prelude::*;
+//!
+//! #[derive(clap::Parser, DotEnvDefault, LoggerDefault, Debug)]
+//! struct Args {
+//!     #[arg(long, env)]
+//!     verbose: bool,
+//! }
+//!
+//! // this function replaces `main`
+//! #[entrypoint::entrypoint]
+//! fn entrypoint(args: Args) -> entrypoint::anyhow::Result<()> {
+//!     // tracing & parsed clap struct are ready-to-use
+//!     info!("entrypoint input args: {:#?}", args);
+//!
+//!     // env vars already have values from dotenv file(s)
+//!     for (key, value) in std::env::vars() {
+//!         println!("{key}: {value}");
+//!     }
+//!
+//!     // easy error propagation
+//!     Ok(())
+//! }
+//! ```
+//!
+//! # Feature Flags
+//! Name     | Description                     | Default?
+//! ---------|---------------------------------|---------
+//! `macros` | Enables optional utility macros | Yes
+//!
 #![forbid(unsafe_code)]
 #![warn(missing_docs, unreachable_pub, unused_crate_dependencies)]
 #![warn(clippy::all, clippy::cargo, clippy::nursery, clippy::pedantic)]
 #![warn(clippy::unwrap_used)]
 
-////////////////////////////////////////////////////////////////////////////////
 pub extern crate anyhow;
 pub extern crate clap;
 pub extern crate tracing;
 pub extern crate tracing_subscriber;
 
-////////////////////////////////////////////////////////////////////////////////
 #[cfg(feature = "macros")]
 pub extern crate entrypoint_macros;
 
+/// re-export [`entrypoint_macros`](https://crates.io/crates/entrypoint_macros)
 #[cfg(feature = "macros")]
 pub mod macros {
     pub use crate::entrypoint_macros::entrypoint;
@@ -22,7 +69,7 @@ pub mod macros {
     pub use crate::entrypoint_macros::LoggerDefault;
 }
 
-////////////////////////////////////////////////////////////////////////////////
+/// essential [traits](#traits) and re-exports
 pub mod prelude {
     pub use crate::anyhow;
     pub use crate::anyhow::Context;
@@ -109,47 +156,74 @@ pub trait DotEnvParser: clap::Parser {
         None
     }
 
+    /// control whether successive dotenv files can override already defined environment variables
+    ///
+    /// Default behavior is to not override.
+    /// This preserves the stock/default [`dotenvy`] behavior.
+    /// Override this default implementation as needed.
+    ///
+    /// # Examples
+    /// ```
+    /// # #[derive(clap::Parser)]
+    /// # struct Args {}
+    /// impl entrypoint::DotEnvParser for Args {
+    ///     fn dotenv_can_override(&self) -> bool { true }
+    /// }
+    /// ```
     fn dotenv_can_override(&self) -> bool {
         false
     }
 
-    /// order matters - env, .env, passed paths
-    /// don't override this
+    /// process dotenv files and populate environment variables
+    ///
+    /// **You probably don't want to override this default implementation.**
+    ///
+    /// # Order Matters!
+    /// Environment variables are processed/set in this order:
+    /// 1. already defined in environment
+    /// 2. `.env` file, if present
+    /// 3. [`additional_dotenv_files`] supplied file(s) (sequentially as supplied)
+    ///
+    /// Keep in mind:
+    /// * Depending on [`dotenv_can_override`], environment variables values may be the first *or* last processed/set.
+    /// * [`additional_dotenv_files`] should be supplied in the order to be processed
+    ///
+    /// # Errors
+    /// * processing an [`additional_dotenv_files`] supplied file fails
     fn process_dotenv_files(self) -> anyhow::Result<Self> {
-        // do twice in case `additional_dotenv_files()` is dependant on `.env`
-        for _ in 0..=1 {
-            let processed_found_dotenv = {
-                if self.dotenv_can_override() {
-                    dotenvy::dotenv_override().map_or(Err(()), |file| {
-                        info!("dotenv::from_filename_override({})", file.display());
-                        Ok(())
-                    })
-                } else {
-                    dotenvy::dotenv().map_or(Err(()), |file| {
-                        info!("dotenv::from_filename({})", file.display());
-                        Ok(())
-                    })
-                }
-            };
-
-            let processed_supplied_dotenv = self.additional_dotenv_files().map_or(Err(()), |files| {
-                for file in files {
-                    if self.dotenv_can_override() {
-                        info!("dotenv::from_filename_override({})", file.display());
-                        dotenvy::from_filename_override(file).or(Err(()))?;
-                    } else {
-                        info!("dotenv::from_filename({})", file.display());
-                        dotenvy::from_filename(file).or(Err(()))?;
-                    }
-                }
-                Ok(())
-            });
-
-            if processed_found_dotenv.is_err() && processed_supplied_dotenv.is_err() {
-                info!("no dotenv file(s) found/processed");
-                break;
-            }
+        if self.dotenv_can_override() {
+            dotenvy::dotenv_override()
+                .map(|file| info!("dotenv::from_filename_override({})", file.display()))
+        } else {
+            dotenvy::dotenv().map(|file| info!("dotenv::from_filename({})", file.display()))
         }
+        .map_err(|_| warn!("no .env file found"))
+        .unwrap_or(()); // suppress, no .env is a valid use case
+
+        self.additional_dotenv_files().map_or(Ok(()), |files| {
+            // try all, so any/all failures will be in the log
+            files.into_iter().fold(Ok(()), |accum, file| {
+                let process = |res: Result<std::path::PathBuf, dotenvy::Error>, msg| {
+                    res.map(|_| info!(msg)).map_err(|e| {
+                        error!(msg);
+                        e
+                    })
+                };
+
+                if self.dotenv_can_override() {
+                    process(
+                        dotenvy::from_filename_override(file.clone()),
+                        format!("dotenv::from_filename_override({})", file.display()),
+                    )
+                } else {
+                    process(
+                        dotenvy::from_filename(file.clone()),
+                        format!("dotenv::from_filename({})", file.display()),
+                    )
+                }
+                .and(accum)
+            })
+        })?; // bail if any of the additional_dotenv_files failed
 
         Ok(self)
     }
